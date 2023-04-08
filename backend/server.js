83285f1fc2b8d7b71a4a3ce952f86714cf7c8cd7
Lucky4604 <luckygovindrao182@gmail.com>
2023-04-08 19:18:45 +0530

    usercontext hook is completed

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,16 @@ const cors = require('cors');
 const mongoose=require('mongoose')
 const User=require('./models/user.js')
 const bcrypt=require('bcryptjs')
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
+
 
 
 
 const app=express();
+const secret = 'kjfkjsdfkjskdf';
+app.use(cookieParser());
+
 const salt=bcrypt.genSaltSync(10);
 app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json())
@@ -26,31 +32,34 @@ app.post('/register',async(req,res)=>{
         res.status(400).json(error)
     }
 })
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      // Find the user with the given username
-      const user = await User.findOne({ username });
-  
-      // If the user doesn't exist, return an error
-      if (!user) {
-        return res.status(400).json({ message: 'Invalid username or password' });
-      }
-  
-      // Compare the user's inputted password with the hashed password in the database
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-  
-      // If the passwords don't match, return an error
-      if (!isPasswordValid) {
-        return res.status(400).json({ message: 'Invalid username or password' });
-      }
-  
-      // If everything checks out, return a success message
-      return res.json({ message: 'Login successful' });
-    } catch (error) {
-      res.status(400).json(error);
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      // logged in
+      jwt.sign({username,id:userDoc._id}, secret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json({
+          id:userDoc._id,
+          username,
+        });
+      });
+    } else {
+      res.status(400).json('wrong credentials');
     }
+  });
+ 
+  app.get('/profile', (req,res) => {
+    const {token} = req.cookies;
+    jwt.verify(token, secret, {}, (err,info) => {
+      if (err) throw err;
+      res.json(info);
+    });
+  });
+  
+  app.post('/logout', (req,res) => {
+    res.cookie('token', '').json('ok');
   });
   
 
